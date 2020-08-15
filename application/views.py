eadb63e5df001f9eb30d2782cfa7bed448f544ee
KarenNgala <ngalalalu@gmail.com>
2020-08-15 15:28:47 +0300

    Add templating, basic urls and views

---
+++ application/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .models import Post, Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+@login_required(login_url='/accounts/login/')
+def feed(request):
+    pictures = Post.objects.all()
+    return render(request, 'index.html',{'pictures':pictures})
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user.profile
+    pics = Post.objects.filter(profile=current_user).all()
+    return render(request, 'profile.html', {'pics':pics})
+
